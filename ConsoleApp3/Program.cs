2ff652a04314f08950a3b6502faa76e611dcea61
stas115gr <stashauzkin10@gmail.com>
2024-12-23 20:10:15 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    /// <summary>
+    /// Вывести в столбик 5 раз слово «Привет!» (циклы с условием)
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadLine();
+        }
+        public static void Func()
+        {
+            int x = 1;
+            while (x < 6)
+            {
+                Console.WriteLine("Привет");
+                x++;
+            }
+        }
+    }
+}
